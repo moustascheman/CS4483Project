e622f956e475866cbb9a587b119a108863f216c2
Rahman Ahmed <rahmed83@uwo.ca>
2021-03-26 14:06:10 -0400

    Boss implemented

---
+++ Assets/Scripts/Enemies/boss1HealthManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss1HealthManager : MonoBehaviour, IHealthManager
+{
+    [SerializeField]
+    private float MaxHealth = 20f;
+    [SerializeField]
+    private float currentHealth = 20f;
+
+    [SerializeField]
+    private EnemyController controller;
+
+    private float currentComboStage = 0;
+    public void Kill()
+    {
+        controller.playAnim("die");
+        Destroy(gameObject, 4f);
+    }
+
+
+    public void KillQuietly()
+    {
+        Destroy(gameObject);
+    }
+
+
+    public void DealDamage(float dam, float hitstun, int comboStage)
+    {
+        if (comboStage > currentComboStage)
+        {
+            //attack hits
+            if ((currentHealth - dam) <= 0)
+            {
+                currentHealth -= dam;
+                Kill();
+            }
+            else
+            {
+                currentHealth -= dam;
+            }
+        }
+    }
+}
